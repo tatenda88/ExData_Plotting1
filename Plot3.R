da590b959981a01053c2748bcdcd3179b0304c17
tatenda88 <69332296+tatenda88@users.noreply.github.com>
2021-01-07 23:37:53 -0500

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,27 @@
+#Create Destination for File
+dest <- "C:/Users/Tatenda/Documents/household_power_consumption.zip"
+
+#Download File, Unzip file and create table
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",dest)
+power <- read.table(unz(dest, "household_power_consumption.txt"),skip=1,sep=";")
+unlink(dest)
+
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
+# calling the basic plot functions
+plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+# annotating graph
+title(main="Energy sub-metering")
+
+#Plot in PNG
+png("Plot3.png)
