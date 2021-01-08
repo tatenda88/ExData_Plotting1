f42d23c10ea67a793a8c12ea357cc0e2f0a18081
tatenda88 <69332296+tatenda88@users.noreply.github.com>
2021-01-08 08:45:44 -0500

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,17 +1,29 @@
-#Create Destination for File
-dest <- "C:/Users/Tatenda/Documents/household_power_consumption.zip"
+
+dest <- "household_power_consumption.zip"
 
 #Download File, Unzip file and create table
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",dest)
 power <- read.table(unz(dest, "household_power_consumption.txt"),skip=1,sep=";")
 unlink(dest)
 
-#Read Table, renam columns and Filter on the 2 days
+#Read Table, rename columns and Filter on the 2 days
 
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+#Plot 2
+
+head(subpower)
+#calling the basic plot function
+
+#clearing plot space
+while (!is.null(dev.list())) dev.off()
+
+# Create PNG
+png("plot4.png",width=480, height=480)
+
+# Transforming the Date and Time from characters to Date 
+
 subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
 subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
 subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
@@ -26,12 +38,12 @@ with(subpower,{
   plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",  xlab="",ylab="Global Active Power")  
   plot(subpower$Time,as.numeric(as.character(subpower$Voltage)), type="l",xlab="datetime",ylab="Voltage")
   plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
-   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
-   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
-   with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
-   legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
+  with(subpower,lines(Time,as.numeric(as.character(Sub_metering_1))))
+  with(subpower,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+  with(subpower,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+  legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
   plot(subpower$Time,as.numeric(as.character(subpower$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
 })
 
-#Plot PNG 
-png("Plot4.png")
+#close plot
+dev.off()
