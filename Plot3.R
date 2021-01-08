c65d5480c0f8d4ad55fe70f580943e33c6563de5
tatenda88 <69332296+tatenda88@users.noreply.github.com>
2021-01-08 08:44:43 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,12 +1,29 @@
-#Create Destination for File
-dest <- "C:/Users/Tatenda/Documents/household_power_consumption.zip"
+
+dest <- "household_power_consumption.zip"
 
 #Download File, Unzip file and create table
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",dest)
 power <- read.table(unz(dest, "household_power_consumption.txt"),skip=1,sep=";")
 unlink(dest)
 
-# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+#Read Table, rename columns and Filter on the 2 days
+
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+#Plot 2
+
+head(subpower)
+#calling the basic plot function
+
+#clearing plot space
+while (!is.null(dev.list())) dev.off()
+
+# Create PNG
+png("plot3.png",width=480, height=480)
+
+# Transforming the Date and Time from characters to Date 
+
 subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
 subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
 subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
@@ -23,6 +40,5 @@ legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1",
 # annotating graph
 title(main="Energy sub-metering")
 
-#Plot in PNG
-png("Plot3.png)
-
+#close plot
+dev.off()
