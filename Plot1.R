3e87937a5c3ddf2142171f761421628d25e3b413
tatenda88 <69332296+tatenda88@users.noreply.github.com>
2021-01-07 23:44:37 -0500

    R code for plot 1

--- Plot1.R
+++ Plot1.R
@@ -5,7 +5,7 @@ download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_p
 power <- read.table(unz(dest, "household_power_consumption.txt"),skip=1,sep=";")
 unlink(dest)
 
-#Read Table, renam columns and Filter on the 2 days
+#Read Table, rename columns and Filter on the 2 days
 
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
