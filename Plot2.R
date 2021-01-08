95b042a63937068fb20b83a375681889b6ddb138
tatenda88 <69332296+tatenda88@users.noreply.github.com>
2021-01-08 08:28:49 -0500

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,11 +1,26 @@
-#Create Destination for File
-dest <- "C:/Users/Tatenda/Documents/household_power_consumption.zip"
+
+dest <- "household_power_consumption.zip"
 
 #Download File, Unzip file and create table
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",dest)
 power <- read.table(unz(dest, "household_power_consumption.txt"),skip=1,sep=";")
 unlink(dest)
 
+#Read Table, rename columns and Filter on the 2 days
+
+names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+
+#Plot 2
+
+head(subpower)
+#calling the basic plot function
+
+#clearing plot space
+while (!is.null(dev.list())) dev.off()
+
+# Create PNG
+png("plot2.png")
 
 # Transforming the Date and Time from characters to Date 
 subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
@@ -20,5 +35,5 @@ plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="
 # annotating graph
 title(main="Global Active Power Vs Time")
 
-#Plot in PNG
-png("Plot1.png") 
+#close plot
+dev.off()
